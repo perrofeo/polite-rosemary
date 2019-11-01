d06a19e18d4d2e10ddc1c6f7ae507affb77a7645
igor <perrofeofilms@gmail.com>
2019-10-31 21:02:09 -0600

    -

--- js/main.js
+++ js/main.js
@@ -699,9 +699,10 @@ $(function() {
   $('#WAButton').floatingWhatsApp({
     phone: '1231231231', //WhatsApp Business phone number International format-
     //Get it with Toky at https://toky.co/en/features/whatsapp.
-    headerTitle: 'Chat with us on WhatsApp!', //Popup Title
-    popupMessage: 'Hello, how can we help you?', //Popup Message
+    headerTitle: 'Ramón Express', //Popup Title
+    popupMessage: 'Hola, quieres ordenar?', //Popup Message
     showPopup: true, //Enables popup display
+    autoOpenTimeout: 2000,
     buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
     //headerColor: 'crimson', //Custom header color
     //backgroundColor: 'crimson', //Custom background button color
