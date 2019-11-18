0870d2d7f66c59c5ac7574c29261e4ad70a10b20
igor <perrofeofilms@gmail.com>
2019-11-18 13:34:32 -0600

    sw

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+//service worker
+if ("serviceWorker" in navigator) {
+  if (navigator.serviceWorker.controller) {
+    console.log("An active service worker found, no need to register");
+  } else {
+    // Register the service worker
+    navigator.serviceWorker
+      .register("/serviceworker.js", {
+        scope: "./"
+      })
+      .then(function (reg) {
+        console.log("Service worker has been registered for scope: " + reg.scope);
+      });
+  }
+}
+
 /*!
  * modernizr v3.6.0
  * Build https://modernizr.com/download?-webp-setclasses-dontmin
